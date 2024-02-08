da95944f1dff5a3bc62d762dfc300c1d14604de9
Sergey Konkin <skonkin@nb-c02gx5crq6lt.barn-sun.ts.net>
2024-02-08 17:11:23 +0100

    initial commit

---
+++ roles/x-c1/files/x-c1-softsd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+   echo "must be run as root"
+   exit 1
+fi
+
+BUTTON=27
+
+echo "$BUTTON" > /sys/class/gpio/export;
+echo "out" > /sys/class/gpio/gpio$BUTTON/direction
+echo "1" > /sys/class/gpio/gpio$BUTTON/value
+
+SLEEP=${1:-4}
+
+re='^[0-9\.]+$'
+if ! [[ $SLEEP =~ $re ]] ; then
+   echo "error: sleep time not a number" >&2; exit 1
+fi
+
+echo "Your device will shutting down in 4 seconds..."
+/bin/sleep $SLEEP
+
+echo "0" > /sys/class/gpio/gpio$BUTTON/value
