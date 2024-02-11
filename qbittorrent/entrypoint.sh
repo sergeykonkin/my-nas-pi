79cb5b95e8b9103a35dbb6f9e887dacbebdbc160
Sergey Konkin <batareeed@gmail.com>
2024-02-11 16:31:42 +0100

    add openvpn client to qbittorrent

--- qbittorrent/entrypoint.sh
+++ qbittorrent/entrypoint.sh
@@ -4,4 +4,12 @@ set -e
 # some magic to redirect logs to stdout
 rm -rf /tmp/qbittorrent.log;
 ln -s -T /proc/self/fd/1 /tmp/qbittorrent.log;
+
+# waiting for openvpn client
+while ! ifconfig | grep "tun0" > /dev/null;
+do
+    echo "[$(date -Iseconds)] waiting for tun0 interface...";
+    sleep 5;
+done;
+
 qbittorrent-nox --profile="/config" --webui-port="8080" $@
